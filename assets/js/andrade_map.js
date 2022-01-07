c1edfef7283ae057bad8c199a5f3b46ed4846dcc
BubuDavid <david.pedroza.segoviano@gmail.com>
2022-01-06 20:00:17 -0600

    Add responsive design

--- assets/js/andrade_map.js
+++ assets/js/andrade_map.js
@@ -192,3 +192,11 @@ Array.from(btns).forEach(btn => {
 		)
 	})
 })
+
+/*================ SCROLLUP ================*/
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 150) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp);
